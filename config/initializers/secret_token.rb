b2dfc359ea8304e2c864f314434641be7211302e
Terrell Green <tgreen@Terrels-MacBook-Pro.local>
2014-01-01 12:57:48 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FsBackend::Application.config.secret_key_base = '56240a5a670b74c4187344a988fd4dac344b27f876ffe6424e57cd4eabc4cbe034482624c70d627072634656efad8f38219dc762da40ba2f93cddc9263bde20e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FsBackend::Application.config.session_store :cookie_store, key: '_fs-backend_session'
